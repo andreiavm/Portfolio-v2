701dd1c481a87a60db7477403b93f2119a483f88
Andrei A <andrei.avm00@gmail.com>
2023-10-04 17:26:11 +0200

    fixed text alignment

--- src/components/cidhub/CidHub.jsx
+++ src/components/cidhub/CidHub.jsx
@@ -9,7 +9,7 @@ const CidHub = () => {
             exit={{ opacity: 0, y: 0, rotate: 0 }}
             transition={{ duration: 0.35 }}>
             <section className="casestudy-section">
-                <h1 style={{ display: 'flex', justifyContent: 'center' }} className="text-header-large">Coming soon! 🦺</h1>
+                <h1 style={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }} className="text-header-large">Coming soon! 🦺</h1>
             </section>
         </motion.div>
     );
